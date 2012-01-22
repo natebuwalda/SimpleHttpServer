3308dd82a533eeba7761a28c5aa0719385c05f62
unknown <Scotch@.(none)>
2012-01-22 13:04:22 -0600

    Nate - changed server work to occur inside a thread

--- src/main/java/com/nate/simplehttpserver/Server.java
+++ src/main/java/com/nate/simplehttpserver/Server.java
@@ -4,17 +4,26 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
 
     private boolean isStarted = false;     
     private ServerSocket serverSocket;
+    private ThreadPoolExecutor threadPoolExecutor;
 
     public boolean start() {
         System.out.println("The server is starting for host 127.0.0.1 on port 8080...");
         try {
             serverSocket = new ServerSocket(8080, 10, InetAddress.getByName("127.0.0.1"));
-            run();
+            BlockingQueue<Runnable> connectionQueue = new LinkedBlockingQueue<Runnable>();
+            threadPoolExecutor = new ThreadPoolExecutor(10, 20, 1000, TimeUnit.MILLISECONDS, connectionQueue);
+            threadPoolExecutor.execute(new ServerWorker());
             isStarted = true;
         } catch (Exception e) {
             System.err.println("The server failed to start properly due to the following reason:");
@@ -29,6 +38,7 @@ public class Server {
         try {
             isStarted = false;
             serverSocket.close();
+            threadPoolExecutor.shutdown();
         } catch (Exception e) {
             System.err.println("The server failed to stop properly due to the following reason:");
             e.printStackTrace();
@@ -36,10 +46,19 @@ public class Server {
         }
         return !isStarted;
     }
-
-    private void run() throws IOException {
-        while(isStarted) {
-            Socket connection = serverSocket.accept();
+    
+    private class ServerWorker implements Runnable {
+        @Override
+        public void run() {
+            while(isStarted) {
+                try {
+                    System.out.println("Now accepting new connections.");
+                    Socket connection = serverSocket.accept();
+                } catch (Exception e) {
+                    System.err.println("The following error occured while the server was running:");
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/test/java/com/nate/simplehttpserver/ServerTest.java
+++ src/test/java/com/nate/simplehttpserver/ServerTest.java
@@ -9,10 +9,12 @@ public class ServerTest {
     private Server testServer = new Server();
 
     @Test
-    public void startAndStopServer() {
+    public void startAndStopServer() throws InterruptedException {
         boolean started = testServer.start();
         assertTrue("Server should be started", started);
 
+        Thread.sleep(1000);
+        
         boolean stopped = testServer.stop();
         assertTrue("Server should be stopped", stopped);
     }
