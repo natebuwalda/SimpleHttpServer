00012b13defb69071349c0e91b4a785d2154bdb3
unknown <Scotch@.(none)>
2012-01-22 12:52:43 -0600

    Nate - combined start and stop tests into one method, added basic server socket config

--- src/main/java/com/nate/simplehttpserver/Server.java
+++ src/main/java/com/nate/simplehttpserver/Server.java
@@ -1,18 +1,45 @@
 package com.nate.simplehttpserver;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-    private boolean isStarted = false;
+    private boolean isStarted = false;     
+    private ServerSocket serverSocket;
 
     public boolean start() {
         System.out.println("The server is starting for host 127.0.0.1 on port 8080...");
-        isStarted = true;
+        try {
+            serverSocket = new ServerSocket(8080, 10, InetAddress.getByName("127.0.0.1"));
+            run();
+            isStarted = true;
+        } catch (Exception e) {
+            System.err.println("The server failed to start properly due to the following reason:");
+            e.printStackTrace();
+            isStarted = false;
+        }
         return isStarted;
     }
 
     public boolean stop() {
         System.out.println("Server is now shutting down...");
-        isStarted = false;
+        try {
+            isStarted = false;
+            serverSocket.close();
+        } catch (Exception e) {
+            System.err.println("The server failed to stop properly due to the following reason:");
+            e.printStackTrace();
+            isStarted = true;
+        }
         return !isStarted;
+    }
+
+    private void run() throws IOException {
+        while(isStarted) {
+            Socket connection = serverSocket.accept();
+        }
     }
 }
